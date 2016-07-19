04c05db5f64fca0f12790604ccf2ab483dc460ed
java <java>
2016-07-19 18:33:45 +0900

    aabaab

--- src/main/java/com/hybrid/MainApplication.java
+++ src/main/java/com/hybrid/MainApplication.java
@@ -2,6 +2,10 @@ package com.hybrid;
 public class MainApplication{
 	public static void main(String[] args){
 		System.out.println("aaaaaaaaaaaaa");
+		for(int i = 0 ; i <10 ; i++){
+			System.out.println("Start~~~~~~~~~~~~~~~~");
+} 
+		System.out.println("aaaaaaaaaaaaa");		
 }
 
 }
